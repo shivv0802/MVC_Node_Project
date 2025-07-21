567f927b1a5af7a5c401519070d5136e2af4913a
Shivam Rai <162560620+shivv0802@users.noreply.github.com>
2025-07-21 10:00:18 +0530

    Update validation.middleware.js

--- middleware/validation.middleware.js
+++ middleware/validation.middleware.js
@@ -1,17 +1,14 @@
 function validateUserData(req, res, next) {
-  const { username, email, password } = req.body;  // Assuming data is in the body
+  const { username, email, password } = req.body; 
 
   const errors = [];
 
-  // Validate username
   
 
-  // Validate email
   if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
     errors.push("Please provide a valid email address.");
   }
 
-  // Validate password
   if (!password || password.length < 6) {
     errors.push("Password must be at least 6 characters long.");
   }
@@ -22,12 +19,10 @@ function validateUserData(req, res, next) {
     errors.push("Password must contain at least one number.");
   }
 
-  // If errors are found, send a response with the errors
   if (errors.length > 0) {
     return res.status(400).json({ errors });
   }
 
-  // If no errors, move to the next middleware or route handler
   next();
 }
 
