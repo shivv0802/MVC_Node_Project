a0fd7de1a6998bec93e664bba2570fc33cd62945
Shivam Rai <shivv9700@gmail.com>
2025-07-21 10:06:23 +0530

    remove index.js and add code into app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // app.js
 const express = require('express');
+
+const connectDb = require('./config/db');
+
+const PORT = 8000;
+
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
@@ -11,7 +16,20 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-
 app.use('/users', userRoutes);
+
+
+const startServer = async () => {
+  try {
+    await connectDb('mongodb://localhost:27017/myappdb');
+    app.listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error('Error connecting to the database:', err.message);
+  }
+};
+
+startServer();
 
 module.exports = app;
