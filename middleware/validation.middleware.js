356cf177f664306d05b7f6512272d36729bf4bdd
Shivam Rai <162560620+shivv0802@users.noreply.github.com>
2025-07-21 12:10:05 +0530

    Update validation.middleware.js
    Commenting the custom validation and inserting the express validation code

--- middleware/validation.middleware.js
+++ middleware/validation.middleware.js
@@ -1,13 +1,32 @@
+const { body, validationResult} = require('express-validator');
+
+
+const validateUserData = [
+  body('email').isEmail().withMessage("enter a valid email"),
+  body('password').isLength({min : 8}).withMessage("Password should be greater than 8 characters"), 
+  (req,res,next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      res.status(400).json({msg : "Invalid Credentials", errors : errors.array()})
+    }
+    next();
+  }
+]
+
+
+/*
+//"This is Custom validation and the above is express validations".
+
 function validateUserData(req, res, next) {
-  const { username, email, password } = req.body; 
+  const { name, email, password } = req.body;  
 
   const errors = [];
 
   
-
   if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
     errors.push("Please provide a valid email address.");
   }
+
 
   if (!password || password.length < 6) {
     errors.push("Password must be at least 6 characters long.");
@@ -24,6 +43,6 @@ function validateUserData(req, res, next) {
   }
 
   next();
-}
+}*/
 
 module.exports = { validateUserData };
